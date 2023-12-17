04cb324e18b4845ed7c4761728a042eb9a97a48f
Bruce Wong <45473259+BruceWong001@users.noreply.github.com>
2023-12-17 16:19:22 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberSkills = document.getElementById("member-skills");
+    var closeSkills = document.getElementById("close-skills");
+    memberSkills.style.display = "block";
+    member.style.display = "none";
+    closeSkills.style.display = "block";
+}
